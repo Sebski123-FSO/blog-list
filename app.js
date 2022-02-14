772aff68eb5b8199f3f02f8659d0a96d65273ae1
sebski123 <sebthomle@gmail.com>
2022-02-14 19:35:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
